d4e7a43c217d9108f2a19c99947a8c5dea0a7c07
Steve Hebert <steve.hebert@gmail.com>
2020-05-03 10:40:24 -0500

    modified to monitor

--- main.go
+++ main.go
@@ -2,36 +2,40 @@ package main
 
 import (
 	"fmt"
-	"os"
+	"strings"
 	"time"
 
+	"github.com/turnage/graw"
 	"github.com/turnage/graw/reddit"
 )
 
-type announcer struct{}
+type reminderBot struct {
+	bot reddit.Bot
+}
 
-func (a *announcer) Post(post *reddit.Post) error {
-	fmt.Printf("%s posted \"%s\"\n", post.Author, post.Title)
+func (r *reminderBot) Post(p *reddit.Post) error {
+	if strings.Contains(p.Title, "Tokyo60") {
+		fmt.Printf("notifying of match at: %s", p.URL)
+		<-time.After(10 * time.Second)
+		return r.bot.SendMessage(
+			p.Author,
+			fmt.Sprintf("Notification: %s", p.Title),
+			fmt.Sprintf("Tokyo60: %s", p.URL),
+		)
+	}
 	return nil
 }
 
 func main() {
-	bot, err := reddit.NewBotFromAgentFile("cred.file", time.Second*15)
-
-	if err != nil {
-		fmt.Println("failed to create bot handle:", err)
-		os.Exit(1)
+	if bot, err := reddit.NewBotFromAgentFile("redmond.agent", 0); err != nil {
+		fmt.Println("Failed to create bot handle: ", err)
+	} else {
+		cfg := graw.Config{Subreddits: []string{"mechmarket"}}
+		handler := &reminderBot{bot: bot}
+		if _, wait, err := graw.Run(handler, bot, cfg); err != nil {
+			fmt.Println("Failed to start graw run: ", err)
+		} else {
+			fmt.Println("graw run failed: ", wait())
+		}
 	}
-
-	harvest, err := bot.Listing("/r/mechmarket", "")
-	if err != nil {
-		fmt.Println("failed to fetch /r/mechmarket: ", err)
-		os.Exit(1)
-
-	}
-
-	for _, post := range harvest.Posts[:5] {
-		fmt.Printf("[%s] posted [%s]\n", post.Author, post.Title)
-	}
-
 }
